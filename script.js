f5dc62bb41b4af859916dcbfaac3a9b0767d0421
udaysathvara <161562665+udaysathvara@users.noreply.github.com>
2024-08-13 10:03:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+// document.getElementById("four_all").style.backgroundColor="red"
+
+// let four_all = document.getElementsByClassName("four_all")
+// four_all[0].style.backgroundColor ="red"
+
+// let icon = document.querySelector('ion-icon');
+// icon.onclick = function(){
+//   icon.classList.toggle('active');
+// }
+
+// let six_first = document.getElementsByClassName("six_first")
+// console.log(six_first)
+// six_first[0].style.marginBottom =  "150px"
+// six_first[1].style.marginBottom= "150px"
+// six_first[2].style.marginBottom= "150px"
+// six_first[3].style.marginBottom= "150px"
+// six_first[4].style.marginBottom= "0px"
+// six_first[5].style.marginBottom= "0px"
+// six_first[6].style.marginBottom= "0px"
+// six_first[7].style.marginBottom= "0px"
